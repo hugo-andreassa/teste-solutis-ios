0eaf9d63a2c0b4c0d4df208b7e67a8a35a6fedfa
Hugo Andreassa <hugo.andreassa@gmail.com>
2021-09-03 17:57:02 -0300

    Reorganizando pastas

--- teste-solutis/Utils/ValidationUtils.swift
+++ teste-solutis/Utils/ValidationUtils.swift
@@ -13,8 +13,11 @@ class ValidationUtils {
         return true
     }
 
-    func isEmail() -> Bool {
-        return true
+    func isEmail(_ email: String) -> Bool {
+        let emailRegEx = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,64}"
+        
+        let emailPred = NSPredicate(format:"SELF MATCHES %@", emailRegEx)
+        return emailPred.evaluate(with: email)
     }
 
     func isPassword() -> Bool {
