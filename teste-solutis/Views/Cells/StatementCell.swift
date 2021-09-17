fbf1c293fba332553692739a343f44e95c7a2f67
Hugo Andreassa <hugo.andreassa@gmail.com>
2021-09-17 13:29:35 -0300

    Corrigindo referencia de arquivo

---
+++ teste-solutis/Views/Cells/StatementCell.swift
@@ -0,0 +1,16 @@
+//
+//  StatementCell.swift
+//  teste-solutis
+//
+//  Created by Virtual Machine on 17/09/21.
+//
+
+import Foundation
+import UIKit
+
+class StatementTableViewCell: UITableViewCell {
+    @IBOutlet weak var typeLabel: UILabel!
+    @IBOutlet weak var descriptionLabel: UILabel!
+    @IBOutlet weak var valueLabel: UILabel!
+    @IBOutlet weak var dateLabel: UILabel!
+}
